d11d7e29fdaf6dbd9363c3b1393752946dae2cb8
uriel Antonio <uantonio@csumb.edu>
2021-07-22 11:01:18 -0700

    updating users

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,11 +4,12 @@ import { AppService } from './app.service';
 import { FoodCategoriesModule } from './components/food-categories/food-categories.module';
 import { FoodItemModule } from './components/food-items/food-item.module';
 import { RestaurantsModule } from './components/restaurants/restaurants.module';
-import { UserModule } from './components/users/user.module';
+import { UsersModule } from './components/users/users.module';
+
 
 
 @Module({
-  imports: [UserModule, RestaurantsModule, FoodCategoriesModule, FoodItemModule],
+  imports: [UsersModule, RestaurantsModule, FoodCategoriesModule, FoodItemModule],
   controllers: [AppController],
   providers: [AppService],
 })
